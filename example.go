7fd2f955c8462f2c4fb8620e5053350b9d30518f
Cenk Deniz <cenk.dnz@gmail.com>
2017-01-04 00:35:50 +0200

    Typo fix.

--- example.go
+++ example.go
@@ -1,31 +1,26 @@
-
 package main
 
-import
+import "fmt"
 
-	"fmt"
-
-func main(){
+func main() {
 
 	var sayı int
 	var sayı1 int
+
 	fmt.Println("1 ile 10 arsında bir sayı söyle")
 
-
-	if (0 <= sayı )&&(sayı<=10){
+	if (0 <= sayı) && (sayı <= 10) {
 		fmt.Scanln(&sayı)
 		fmt.Println("bu sayıyı hangi sayıyla çarpmamı istersin")
 		fmt.Scanln(&sayı1)
 		var sonuc int
-		sonuc = carp(sayı,sayı1)
+		sonuc = carp(sayı, sayı1)
 		fmt.Printf("bu iki sayının çarpımının sonucu %d dir", sonuc)
-	}else if ( sayı<= 10)&& (sayı==0){
-
+	} else if ( sayı <= 10)&& (sayı == 0) {
 		fmt.Println("lütfen 1 ile 10 arasında bir raka söyleyin")
-
 	}
 }
 
-func carp(x int, y int)(int){
-
-	return x*y }
+func carp(x int, y int) (int) {
+	return x * y
+}
